43bf69dc9c6c658ec72cad838ff04e584f8a1061
Cesar Augusto Nogueira <cesarnogueira1210@gmail.com>
2017-08-29 17:33:10 -0300

    Reverting changes

--- src/components/evento.js
+++ src/components/evento.js
@@ -8,7 +8,7 @@ function formatDate (time) {
 
 export const Evento = ({ info, comunidade, local }) => (
   <div>    
-    <!--<p>Nome do grupo: {comunidade[info.group]}</p>-->
+    {/* <p>Nome do grupo: {comunidade[info.group]}</p> */}
     <strong>{info.name}</strong><br/>
     <ul>
       <li>Data: {formatDate(info.time)}</li>
