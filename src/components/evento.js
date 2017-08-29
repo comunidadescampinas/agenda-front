4bbefe5f106cafe2ee5d90e4d2d9fb76b6f9b40b
Cesar Augusto Nogueira <cesarnogueira1210@gmail.com>
2017-08-29 17:51:24 -0300

    Testing Console #2

--- src/components/evento.js
+++ src/components/evento.js
@@ -6,9 +6,7 @@ function formatDate (time) {
   return moment(time).format('DD/MM/YYYY hh:mm:ss')
 }
 
-export const Evento = ({ info, comunidade, local }) => (
-  console.log(info)
-  console.log(comunidade)
+export const Evento = ({ info, comunidade, local }) => (  
   console.log(comunidade[info.group])
   <div>
     <strong>{info.name}</strong><br/>
