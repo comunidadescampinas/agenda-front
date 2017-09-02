4856d9e4b3aa9e5183716b875c2cd2bb820812c8
Filipe Oliveira <contato@fmoliveira.com.br>
2017-09-01 23:24:28 -0300

    Instalação do Material UI.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+/* Libraries */
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+/* Styles */
 import './index.css';
+import 'typeface-roboto';
+
+/* App */
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+/* Startup */
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
